08e0c8e35d77e761d5038bb4a7b82c3cee4c5fb9
Colin Vaughn <Colinvaughn01@gmail.com>
2023-07-04 04:55:27 -0400

    math

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    java
 }
 
 group = "org.example"
@@ -7,6 +7,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "org.example.Main"
+    }
 }
 
 dependencies {
